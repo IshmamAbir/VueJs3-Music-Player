24d37bea5ebab1d36364693395d2784c9b9424b3
イシュマム | Ishmam <i-chowdhury@kodensya.co.jp>
2023-03-17 11:08:30 +0900

    setup: rules & errorMessage for vee-validate

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -80,15 +80,17 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
+                name="name"
                 type="text"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -147,7 +149,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -168,7 +170,10 @@ export default {
   },
   data() {
     return {
-      tab: 'login'
+      tab: 'login',
+      schema: {
+        name: 'required'
+      }
     }
   }
 }
