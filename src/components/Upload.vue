ba341ae115d12c6e51aea720f9e7cfdfbb557e5d
イシュマム | Ishmam <ishmam130@gmail.com>
2023-03-23 11:18:24 +0900

    add: file upload

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -21,15 +21,22 @@
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm" :class="upload.text_class">
+          <i :class="upload.icon"></i>
+          {{ upload.name }}
+        </div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
-          <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
+          <div
+            class="transition-all progress-bar"
+            :style="{ width: upload.current_progress + '%' }"
+            :class="upload.variant"
+          ></div>
         </div>
       </div>
-      <div class="mb-4">
+      <!-- <div class="mb-4">
         <div class="font-bold text-sm">Just another song.mp3</div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
@@ -40,17 +47,20 @@
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
+import { storage, auth, songsCollection } from '@/includes/firebase'
+
 export default {
   name: 'Upload',
   data() {
     return {
-      is_dragover: false
+      is_dragover: false,
+      uploads: []
     }
   },
   methods: {
@@ -61,6 +71,50 @@ export default {
         if (file.type !== 'audio/mpeg') {
           return
         }
+        const storageRef = storage.ref('songs')
+        const songsRef = storageRef.child(`songs/${file.name}`)
+        const task = songsRef.put(file)
+
+        const uploadIndex =
+          this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+            variant: 'bg-blue-400',
+            icon: 'fas fa-spinner fa-spin',
+            text_class: ''
+          }) - 1
+
+        task.on(
+          'state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            this.uploads[uploadIndex].current_progress = progress
+          },
+          (error) => {
+            this.uploads[uploadIndex].variant = 'bg-red-400'
+            this.uploads[uploadIndex].icon = 'fas fa-times'
+            this.uploads[uploadIndex].text_class = 'text-red-400'
+            console.log(error)
+          },
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              display_name: auth.currentUser.displayName,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: '',
+              comment_count: 0
+            }
+
+            song.url = await task.snapshot.ref.getDownloadURL()
+            songsCollection.add(song)
+
+            this.uploads[uploadIndex].variant = 'bg-green-400'
+            this.uploads[uploadIndex].icon = 'fas fa-check'
+            this.uploads[uploadIndex].text_class = 'text-green-400'
+          }
+        )
       })
       console.log(files)
     }
