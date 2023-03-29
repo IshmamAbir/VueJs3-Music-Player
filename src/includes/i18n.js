d12adbc7111a3df2b26317aa87d24d488b1541d4
イシュマム | Ishmam <ishmam130@gmail.com>
2023-03-29 11:28:38 +0900

    refactor: i18n setup

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
+import ja from '@/locales/ja.json'
 
 export default createI18n({
   locale: 'en',
   fallbackLocale: 'ja',
   messages: {
-    en
+    en,
+    ja
   }
 })
