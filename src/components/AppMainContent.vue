450f34900b5ad47f053ec0794d9cc0b57b379457
イシュマム | Ishmam <ishmam130@gmail.com>
2023-03-29 10:36:29 +0900

    add: custom directives with passing values

--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="container mx-auto">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon="'headphones-alt'">
+        <!-- 'v-icon:full' to use an alternative class rather than the directives default -->
         <span class="card-title">Songs</span>
         <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
       </div>
       <!-- Playlist -->
       <ol id="playlist">
